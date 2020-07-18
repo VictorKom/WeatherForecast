ac24c8269acfd75919741e19c44815b0350870b3
JohnWh1te <butek240594@mail.ru>
2020-07-18 11:17:48 +0400

    add Dagger injection

--- app/src/main/java/com/myapps/weather/data/RemoteRepository.kt
+++ app/src/main/java/com/myapps/weather/data/RemoteRepository.kt
@@ -1,13 +1,14 @@
 package com.myapps.weather.data
 
-import com.myapps.weather.network.CurrentWeather
-import com.myapps.weather.network.ForecastByTime
-import com.myapps.weather.network.WeatherApi
+import com.myapps.weather.data.network.api.WeatherApi
+import com.myapps.weather.data.network.CurrentWeather
+import com.myapps.weather.data.network.ForecastByTime
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
 
-class RemoteRepository constructor(private val weatherApi: WeatherApi ){
+class RemoteRepository @Inject constructor(private val weatherApi: WeatherApi){
 
     fun requestCurrentWeather(latitude: String, longitude: String): Single<CurrentWeather> {
         return weatherApi.getCurrentWeather(latitude, longitude)
